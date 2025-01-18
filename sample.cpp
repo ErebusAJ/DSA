28603a4814dbfa76eb65645d51a6fdd196b1d0b3
Aarya <falconblaze90@gmail.com>
2025-01-18 19:13:39 +0530

    Linked Lists

--- sample.cpp
+++ sample.cpp
@@ -1,43 +1,97 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+# include<bits/stdc++.h>
 
 using namespace std;
 
-vector<int> rowAndMaximumOnes(vector<vector<int>> &mat)
-{
-    int row = mat.size();
-    int column = mat[0].size();
-    int count = 0, index = 0;
-    for (int i = 0; i < row; i++)
-    {
-        int temp = 0;
-        for (int j = 0; j < column; j++)
-        {
-            if (mat[i][j] == 1)
-            {
-                temp++;
-            }
-        }
-        if (count == temp)
-        {
-            continue;
-        }
-        if (count < temp)
-        {
-            count = temp;
-            index = i;
-        }
-    }
+class Node{
+public:
+    int data;
+    Node *next;
 
-    return {index, count};
+    Node(){}
+
+    Node(int d){
+        this->data = d;
+        this->next = NULL;
+    }
+};
+
+void insertAtHead(Node *&head, int d){
+    Node *toAdd = new Node(d);  
+    toAdd->next = head;
+    head = toAdd;
 }
 
-int main()
-{
-    vector<vector<int>> num{{0, 1}, {1, 0}};
-    auto result = rowAndMaximumOnes(num);
-    for(int i : result){
-        cout << i << " ";
+void insertAtTail(Node *&tail, int d){
+    Node *toAdd = new Node(d);
+    tail->next = toAdd;
+    tail = toAdd;
+}
+
+void insertAtMid(Node *head, int d, int p){
+    Node *toAdd = new Node(d);
+    Node *temp = head;
+    int ctr = 0;
+    while(temp != NULL){
+        ctr++;
+        if(ctr == p - 1) break;
+        temp = temp->next;
     }
+
+    toAdd->next = temp->next;
+    temp->next = toAdd;
+
+}
+
+void printLL(Node *&head){
+    Node *temp = head;
+    while(temp != NULL){
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+}
+
+int sizeLL(Node *&head){
+    Node *temp = head;
+    int ctr = 0;
+    while(temp != NULL){
+        ctr++;
+        temp = temp->next;
+    }
+
+    return ctr;
+}
+
+Node *insertAtEnd(Node *head, int x) {
+        Node *toAdd = new Node(x);
+        if(head->next == NULL){
+            head = toAdd;
+            return head;
+        }
+        Node *temp = head;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        
+        temp->next = toAdd;
+        
+        return head;
+    }
+
+int main(){
+    Node *head = new Node();
+    // Node *tail = head;
+
+
+    // insertAtHead(head, 20);
+    // insertAtTail(tail, 30);
+    // insertAtMid(head, 40, 3);
+
+    // printLL(head);
+
+    // cout << "Size: " << sizeLL(head) << endl;
+
+    Node *newH = insertAtEnd(head, 345);
+    printLL(newH);
+
+
 }
